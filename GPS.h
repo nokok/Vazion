e6e89a8f43c20183f73b3dc4c78c111ecfa8934c
nokok <nokok.kz@gmail.com>
2013-08-22 18:07:52 +0900

    Add File :GPS

---
+++ GPS.h
@@ -0,0 +1,13 @@
+//
+//  GPS.h
+//  Vazion
+//
+//  Created by noko on 2013/08/22.
+//  Copyright (c) 2013年 noko. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GPS : NSObject
+
+@end
---
+++ GPS.m
@@ -0,0 +1,13 @@
+//
+//  GPS.m
+//  Vazion
+//
+//  Created by noko on 2013/08/22.
+//  Copyright (c) 2013年 noko. All rights reserved.
+//
+
+#import "GPS.h"
+
+@implementation GPS
+
+@end
