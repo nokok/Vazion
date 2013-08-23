7a6bdc147767c60f64a1e184ead44aa5b377a9a4
Yassine <yassine.bouhmadi.jp@gmail.com>
2013-08-23 10:15:32 +0900

    Update GPS

--- Vazion/GPS.h
+++ Vazion/GPS.h
@@ -7,7 +7,12 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <CoreLocation/CoreLocation.h>
+
+@class CLLocation;
 
 @interface GPS : NSObject
+
+-(Boolean)isGPSEnabled;
 
 @end
--- Vazion/GPS.m
+++ Vazion/GPS.m
@@ -8,6 +8,23 @@
 
 #import "GPS.h"
 
-@implementation GPS
+@class CLLocationManager;
+
+@implementation GPS{
+@private
+    CLLocationManager *locationManager;
+}
+
+-(id)init{
+    self = [super init];
+    if(self){
+        locationManager = [[CLLocationManager alloc]init];
+    }
+    return self;
+}
+
+-(Boolean)isGPSEnabled{
+    return [CLLocationManager locationServicesEnabled];
+}
 
 @end
